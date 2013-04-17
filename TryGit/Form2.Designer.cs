e4d7d6b1b33bb23652f275a1c3b987e0addb076b
gypsyShovan <gypsy.shovan@gmail.com>
2013-04-18 00:13:15 +0600

    Test commit

--- TryGit/Form2.Designer.cs
+++ TryGit/Form2.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TryGit
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.Text = "Form2";
         }
 
         #endregion
